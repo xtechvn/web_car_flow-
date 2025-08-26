93e6c3516dd7ee1850b4772d9665d57070fbd6c1
Hiếu <62824420+anhhieuk16@users.noreply.github.com>
2025-08-26 14:45:59 +0700

    update

--- XTECH-FRONTEND/Program.cs
+++ XTECH-FRONTEND/Program.cs
@@ -42,62 +42,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.MapControllerRoute(
-    name: "News",
-    pattern: "/dang-ky-dang-nhap",
-    defaults: new { controller = "LoginOrRegister", action = "Index" });
-app.MapControllerRoute(
-    name: "News",
-    pattern: "/tin-tuc",
-    defaults: new { controller = "News", action = "Index" });
-app.MapControllerRoute(
-    name: "News",
-    pattern: "/tin-tuc/{id}",
-    defaults: new { controller = "News", action = "Detail" });
-app.MapControllerRoute(
-    name: "AboutUs",
-    pattern: "/ve-chung-toi",
-    defaults: new { controller = "AboutUs", action = "Index" });
-app.MapControllerRoute(
-    name: "pricing",
-    pattern: "/bang-gia-thiet-ke-website",
-    defaults: new { controller = "Pricing", action = "Index" });
-app.MapControllerRoute(
-    name: "home",
-    pattern: "/trang-chu",
-    defaults: new { controller = "Home", action = "Index" });
-app.MapControllerRoute(
-    name: "Contact",
-    pattern: "/lien-he",
-    defaults: new { controller = "Contact", action = "Index" });
-app.MapControllerRoute(
-    name: "faq",
-    pattern: "/dich-vu",
-    defaults: new { controller = "FAQ", action = "Index" });
-app.MapControllerRoute(
-    name: "faq",
-    pattern: "/dich-vu/chi-tiet-du-an",
-    defaults: new { controller = "FAQ", action = "DetailDeginwebsite" });
-app.MapControllerRoute(
-    name: "faq",
-    pattern: "/dich-vu/vps",
-    defaults: new { controller = "FAQ", action = "DetailVPS" });
-app.MapControllerRoute(
-    name: "faq",
-    pattern: "/dich-vu/dang-ky-vps",
-    defaults: new { controller = "FAQ", action = "DangkyVPS" });
-app.MapControllerRoute(
-    name: "faq",
-    pattern: "/dich-vu/crawl-api",
-    defaults: new { controller = "FAQ", action = "CrawlAPIindex" });
-app.MapControllerRoute(
-    name: "home",
-    pattern: "/",
-    defaults: new { controller = "Home", action = "Index" });
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
+
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
