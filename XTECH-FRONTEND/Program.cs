2b5d10e0c25e17ab3b895cb36d85dca7e6e1cd6e
Hiếu <62824420+anhhieuk16@users.noreply.github.com>
2025-08-26 22:59:42 +0700

    update

--- XTECH-FRONTEND/Program.cs
+++ XTECH-FRONTEND/Program.cs
@@ -42,6 +42,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapControllerRoute(
+    name: "home",
+    pattern: "/",
+    defaults: new { controller = "Home", action = "Index" });
 
 
 app.UseCors("AllowAll");
