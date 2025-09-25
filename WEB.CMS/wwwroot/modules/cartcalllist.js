e23a9f8754dd4145b79304b42f2dc3cf29dedee7
xtechvn <nguyendang01092003@gmail.com>
2025-09-25 11:50:40 +0700

    updateList

---
+++ WEB.CMS/wwwroot/modules/cartcalllist.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    _cartofactory.init();
+    var input_chua_xu_ly = document.getElementById("input_chua_xu_ly");
+    input_chua_xu_ly.addEventListener("keypress", function (event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            _cartofactory.ListCartoFactory();
+        }
+    });
+    var input_da_xu_ly = document.getElementById("input_da_xu_ly");
+    input_da_xu_ly.addEventListener("keypress", function (event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            _cartofactory.ListCartoFactory_Da_SL();
+        }
+    });
+});
+var _cartofactory = {
+    init: function () {
+        _cartofactory.ListCartoFactory();
+        _cartofactory.ListCartoFactory_Da_SL();
+    },
+    ListCartoFactory: function () {
+        var model = {
+            VehicleNumber: $('#input_da_xu_ly').val(),
+            PhoneNumber: $('#input_da_xu_ly').val(),
+            VehicleStatus: null,
+            LoadType: null,
+            VehicleWeighingType: null,
+            VehicleTroughStatus: null,
+            TroughType: null,
+            VehicleWeighingStatus: null,
+        }
+        $.ajax({
+            url: "/ListCar/ListCarCallView",
+            type: "post",
+            data: { SearchModel: model },
+            success: function (result) {
+                $('#imgLoading').hide();
+                $('#data_chua_xu_ly').html(result);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+            }
+        });
+    },
+    ListCartoFactory_Da_SL: function () {
+        var model = {
+            VehicleNumber: $('#input_da_xu_ly').val(),
+            PhoneNumber: $('#input_da_xu_ly').val(),
+            VehicleStatus: 0,
+            LoadType: null,
+            VehicleWeighingType: null,
+            VehicleTroughStatus: null,
+            TroughType: null,
+            VehicleWeighingStatus: null,
+        }
+        $.ajax({
+            url: "/ListCar/ListCarCallView",
+            type: "post",
+            data: { SearchModel: model },
+            success: function (result) {
+                $('#imgLoading').hide();
+                $('#data_da_xu_ly').html(result);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+            }
+        });
+    }
+}
