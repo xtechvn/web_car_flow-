1f45e1b3dca82f0f552c0873159ea654a31fdec2
Hiếu <anhhieuk50@gmail.com>
2025-10-06 14:49:05 +0700

    update

--- WEB.CMS/Controllers/CarController.cs
+++ WEB.CMS/Controllers/CarController.cs
@@ -486,7 +486,7 @@ namespace WEB.CMS.Controllers
                         break;
                     case 9:
                         {
-                            if ( model.VehicleTroughStatus != (int)VehicleTroughStatus.Blank)
+                            if ( model.VehicleTroughStatus != (int)VehicleTroughStatus.Blank && model.VehicleTroughStatus != null)
                             {
                                 return Ok(new
                                 {
