4e1f3490a535dcf2c27376c0dd7cd64ef18bb724
Hiếu <62824420+anhhieuk16@users.noreply.github.com>
2025-10-07 08:56:34 +0700

    js

--- WEB.CMS/wwwroot/modules/listVehicles.js
+++ WEB.CMS/wwwroot/modules/listVehicles.js
@@ -195,10 +195,10 @@
         var date = new Date(item.vehicleWeighingTimeComplete);
         let formatted =
             String(date.getHours()).padStart(2, '0') + ":" +
-            String(date.getMinutes()).padStart(2, '0') + ":" +
+            String(date.getMinutes()).padStart(2, '0') + " " +
             String(date.getDate()).padStart(2, '0') + "/" +
             String(date.getMonth() + 1).padStart(2, '0') + "/" +
-            date.getFullYear() + " " ;
+            date.getFullYear()  ;
         return `
         <tr class="CartoFactory_${item.id}" data-queue="${item.recordNumber}" >
             <td>${item.recordNumber}</td>
