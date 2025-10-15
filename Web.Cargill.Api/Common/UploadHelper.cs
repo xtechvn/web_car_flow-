a9279833e489444b198a2c3ca90b7bdd7cf10f71
xtechvn <nguyendang01092003@gmail.com>
2025-10-15 21:19:04 +0700

    updateName

--- Web.Cargill.Api/Common/UploadHelper.cs
+++ Web.Cargill.Api/Common/UploadHelper.cs
@@ -30,44 +30,33 @@ namespace B2B.Utilities.Common
         static string AES_KEY = "bXny0OMniop5U1fpZ6u7zAHg7KxRdUcp7OuhU7L9Oo62k+FLShyDuwjBGuXWtRMK";
         static string AES_IV = "KFavGEDPdhddqjl9CQVC2c0jYoMJKzmqlBDS+JBbSK6QwgG79XWs9ltH0i5DaJm2";
 
-        public static async Task<string?> UploadFileOrImage(IFormFile file, long dataId, int type)
+        public static async Task<string?> UploadFileOrImage(IFormFile file, long dataId, int type, string? customFileName = null)
         {
             if (file == null || file.Length <= 0)
                 throw new Exception("File không hợp lệ.");
 
             try
             {
-                // Danh sách phần mở rộng hợp lệ
-                var validImageExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
-                var validFileExtensions = new[] { ".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx" };
                 var validAudioExtensions = new[] { ".mp3", ".wav", ".m4a", ".mpga" };
-
-                // Lấy extension gốc
                 var extension = Path.GetExtension(file.FileName).ToLower();
+                if (extension == ".mpga") extension = ".mp3";
 
-                // Nếu là .mpga thì chuẩn hóa thành .mp3
-                if (extension == ".mpga")
-                {
-                    extension = ".mp3";
-                }
+                // Dùng custom file name nếu có
+                string fileName = !string.IsNullOrEmpty(customFileName)
+                    ? $"{customFileName}{extension}"
+                    : Path.GetFileNameWithoutExtension(file.FileName) + extension;
 
-                // Tạo lại tên file chuẩn hóa
-                var fileName = Path.GetFileNameWithoutExtension(file.FileName) + extension;
-
-                // Validate extension
-                if (!validImageExtensions.Contains(extension)
-                    && !validFileExtensions.Contains(extension)
-                    && !validAudioExtensions.Contains(extension))
-                {
+                // Validate định dạng
+                var validExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx" }
+                    .Concat(validAudioExtensions);
+                if (!validExtensions.Contains(extension))
                     throw new Exception($"Định dạng file {extension} không được hỗ trợ.");
-                }
 
                 // Sinh token
                 byte[] AESKey = EncryptService.Get_AESKey(EncryptService.ConvertBase64StringToByte(AES_KEY));
                 byte[] AESIV = EncryptService.Get_AESIV(EncryptService.ConvertBase64StringToByte(AES_IV));
                 string token = GenerateToken(AESKey, AESIV);
 
-                // Chuẩn bị form-data
                 using var formData = new MultipartFormDataContent();
                 using var fileStream = file.OpenReadStream();
 
@@ -77,18 +66,15 @@ namespace B2B.Utilities.Common
                 formData.Add(new StringContent(type.ToString()), "type");
                 formData.Add(new StringContent(token), "token");
 
-                // Gửi request lên API static server
                 using var httpClient = new HttpClient();
                 var response = await httpClient.PostAsync(apiUploadFile, formData);
                 var responseContent = await response.Content.ReadAsStringAsync();
 
-                // Debug log để kiểm tra response thực tế
                 LogHelper.InsertLogTelegram($"UploadFileOrImage Response: {responseContent}");
 
                 dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
                 if (response.IsSuccessStatusCode && jsonResponse?.status == 0)
                 {
-                    // Trả về URL public
                     return $"https://static-image.adavigo.com{jsonResponse.url}";
                 }
 
@@ -101,6 +87,7 @@ namespace B2B.Utilities.Common
                 throw;
             }
         }
+
 
 
 
