cc9f8e7649b407417541e95dcc7385312c9edb23
Hiếu <anhhieuk50@gmail.com>
2025-12-15 10:14:15 +0700

    update lưu mongo vs check biển số xe

---
+++ XTECH-FRONTEND/Services/BackgroundQueue/InsertQueue.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Channels;
+using XTECH_FRONTEND.Model;
+namespace XTECH_FRONTEND.Services.BackgroundQueue
+{
+    public class InsertQueue : IInsertQueue
+    {
+        private readonly Channel<InsertJob> _queue;
+
+        public InsertQueue()
+        {
+            _queue = Channel.CreateUnbounded<InsertJob>();
+        }
+
+        public bool Enqueue(InsertJob job)
+        {
+            return _queue.Writer.TryWrite(job);
+        }
+
+        public ValueTask<InsertJob> DequeueAsync(CancellationToken cancellationToken)
+            => _queue.Reader.ReadAsync(cancellationToken);
+    }
+}
