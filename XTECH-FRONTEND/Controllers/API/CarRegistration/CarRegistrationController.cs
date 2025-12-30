2f838f07f105aac5f69bac5b1df5729c31a54e5f
Hiếu <anhhieuk50@gmail.com>
2025-12-30 09:24:22 +0700

    update bỏ log khi tạo  fe

--- XTECH-FRONTEND/Controllers/API/CarRegistration/CarRegistrationController.cs
+++ XTECH-FRONTEND/Controllers/API/CarRegistration/CarRegistrationController.cs
@@ -368,22 +368,22 @@ namespace XTECH_FRONTEND.Controllers.CarRegistration
         {
             try
             {
-                var message = $"✅ ĐĂNG KÝ XE THÀNH CÔNG!\n\n" +
-                $" Tên khách hàng: {request.Name}\n" +
-                $"📱 Số điện thoại: {request.PhoneNumber}\n" +
-                $"🚗 Biển số xe: {request.PlateNumber}\n" +
-                $"🚗 Trọng tải xe: {request.Referee}\n" +
-                $"🎫 Hoàn hảo/Trại : {request.GPLX}\n" +
-                $"🎫 Số thứ tự của bạn: {request.QueueNumber:D3}\n" +
-                $"⏰ Thời gian đăng ký: {request.RegistrationTime:dd/MM/yyyy HH:mm}\n\n" +
-                $"📍 VUI LÒNG:\n" +
-                $"• Chuẩn bị đầy đủ giấy tờ xe\n" +
-                $"• Có mặt đúng giờ theo thứ tự\n" +
-                $"• Theo dõi cập nhật qua Zalo\n\n" +
-                $"🔔 Chúng tôi sẽ thông báo khi đến lượt bạn!\n\n" +
-                $"📞 Hotline hỗ trợ: 1900-1234\n" +
-                $"🌐 Website: https://cargillhanam.com\n\n" +
-                $"Cảm ơn bạn đã sử dụng dịch vụ! ";
+                //var message = $"✅ ĐĂNG KÝ XE THÀNH CÔNG!\n\n" +
+                //$" Tên khách hàng: {request.Name}\n" +
+                //$"📱 Số điện thoại: {request.PhoneNumber}\n" +
+                //$"🚗 Biển số xe: {request.PlateNumber}\n" +
+                //$"🚗 Trọng tải xe: {request.Referee}\n" +
+                //$"🎫 Hoàn hảo/Trại : {request.GPLX}\n" +
+                //$"🎫 Số thứ tự của bạn: {request.QueueNumber:D3}\n" +
+                //$"⏰ Thời gian đăng ký: {request.RegistrationTime:dd/MM/yyyy HH:mm}\n\n" +
+                //$"📍 VUI LÒNG:\n" +
+                //$"• Chuẩn bị đầy đủ giấy tờ xe\n" +
+                //$"• Có mặt đúng giờ theo thứ tự\n" +
+                //$"• Theo dõi cập nhật qua Zalo\n\n" +
+                //$"🔔 Chúng tôi sẽ thông báo khi đến lượt bạn!\n\n" +
+                //$"📞 Hotline hỗ trợ: 1900-1234\n" +
+                //$"🌐 Website: https://cargillhanam.com\n\n" +
+                //$"Cảm ơn bạn đã sử dụng dịch vụ! ";
                 //string url = "https://api-cargillhanam.adavigo.com/api/vehicleInspection/insert";
                 //var client = new HttpClient();
                 //var request_api = new HttpRequestMessage(HttpMethod.Post, url);
@@ -397,7 +397,7 @@ namespace XTECH_FRONTEND.Controllers.CarRegistration
                 //{
                 //    LogHelper.InsertLogTelegram("Insert - lỗi " );
                 //}
-                LogHelper.InsertLogTelegram(message);
+                //LogHelper.InsertLogTelegram(message);
                 return StatusCode(200, "thành công");
             }
             catch (Exception ex)
